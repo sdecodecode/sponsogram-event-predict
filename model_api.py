941f322c4aba18c697191d7aadcb227d4cd1002a
Devanshu <design.devanshu@gmail.com>
2024-09-18 18:29:05 +0530

    Refactor model loading and add API test endpoint

--- model_api.py
+++ model_api.py
@@ -1,39 +1,30 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import pickle
+import joblib
 import pandas as pd
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
 
-with open('sponsor_roi_model.pkl', 'rb') as f:
-    model = pickle.load(f)
-with open('scaler1.pkl', 'rb') as f:
-    scaler = pickle.load(f)
+# Load model and scaler
+model = joblib.load('sponsor_roi_model.pkl')
+scaler = joblib.load('scaler1.pkl')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
 
     input_features = {
-            'Event Type': data['event_type'],
-            'Event Duration in Days': int(data['event_duration']),
-            'Expected Footfall': (int(data['expected_max_footfall']) + int(data['expected_min_footfall'])) / 2,
-            'Ticket Price': float(data['ticket_price']),
-            'Sponsor Type': data['sponsor_type'],
-            'Sponsor Cost': float(data['sponsor_cost'])
-        }
-        # logger.debug(f"Input features: {input_features}")
-    input_df = pd.DataFrame([input_features])   
-    # event_type = data['event_type']
-    # event_duration = int(data['event_duration']),
-    # expected_footfall = (int(data['expected_max_footfall']) + int(data['expected_min_footfall'])) / 2,
-    # ticket_price = float(data['ticket_price']),
-    # sponsor_type = data['sponsor_type'],
-    # sponsor_cost = float(data['sponsor_cost'])
-
-    # input_features = np.array([[event_type, event_duration, expected_footfall, ticket_price, sponsor_type, sponsor_cost]])
+        'Event Type': data['event_type'],
+        'Event Duration in Days': int(data['event_duration']),
+        'Expected Footfall': (int(data['expected_max_footfall']) + int(data['expected_min_footfall'])) / 2,
+        'Ticket Price': float(data['ticket_price']),
+        'Sponsor Type': data['sponsor_type'],
+        'Sponsor Cost': float(data['sponsor_cost'])
+    }
+    input_df = pd.DataFrame([input_features])
 
     predicted_revenue = model.predict(input_df)[0]
     sponsor_cost = input_df['Sponsor Cost'].values[0]
@@ -41,10 +32,16 @@ def predict():
     scaled_roi = scaler.transform([[roi]])[0][0]
     return jsonify({"predicted_revenue": float(predicted_revenue), "roi": float(roi), "scaled_roi": float(scaled_roi)})
 
+
 @app.route('/test', methods=['GET'])
 def api_test():
-    response_data = {"message": "Model API is working :)!"}
-    return jsonify(response_data), 200 
-    
+    response_data = {
+        'message': 'Model API is working fine :)',
+        'success': 'true',
+        'status': 200,
+    }
+    return jsonify(response_data), 200
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
